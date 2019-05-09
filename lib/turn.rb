7c7722b46e3d5819d74cbd974f5c3b3dba5ae6e1
Trish Ogen <patriciaogen@gmail.com>
2019-05-09 13:00:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,10 @@
-def displaY_board(board)
-  
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end 
 
 def valid_move?(board, index)
   if index.between?(0,8) == true && position_taken?(board, index) == false
@@ -18,3 +23,7 @@ def position_taken?(board, index)
     false
   end
 end
+
+def move(board, index, token = 'X')
+  
+  
