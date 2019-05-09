5c8c0600aaaafeb397929d09a7191395557966e9
Trish Ogen <patriciaogen@gmail.com>
2019-05-09 13:03:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end 
 
+def input_to_index(input)
+  index = input - 1
+end
+
 def valid_move?(board, index)
   if index.between?(0,8) == true && position_taken?(board, index) == false
     true
